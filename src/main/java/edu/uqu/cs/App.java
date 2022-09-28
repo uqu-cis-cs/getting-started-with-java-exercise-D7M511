81f43eb01a9987b2e5828f5ca00971c1a208ab93
Abdulrahman Faisal <s443004607@st.uqu.edu.sa>
2022-09-28 20:56:50 +0300

    my first git commit

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -14,6 +14,6 @@ package edu.uqu.cs;
 public class App{
     public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Abdulrahman , 443004607" );
     }
 }
